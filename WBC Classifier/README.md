a29f17ba322621a86927fc5925e3eefeb4b64580
Adam <astogsdill@mymail.mines.edu>
2020-06-24 20:21:39 -0600

    Documentation and dataset analysis

---
+++ WBC Classifier/README.md
@@ -0,0 +1,36 @@
+# Wisconson Breast Cancer Dataset
+
+### Dataset Metrics
+
+#### Class Distribution
+
+Malignant: 37.258%
+Bengin: 62.742%
+
+In the figure below you can see the number of occurrences fo each class
+![Number of Occurrences for each class](class_distribution.png)
+
+In the figure below you can see the distribution of values for each row.
+The number of outliers most likely indicates/correlates with the malignant
+cases.
+![Data Distribution](data_distribution.png)
+
+### AI Model
+
+The model that I chose to implement was sklearn's Decision Tree model.
+In the figure below you can see a visualization of the model.
+![visualization](classifier_visualization.png)
+
+Accuracy: 0.956140350877193
+
+Precision: 0.9848484848484849
+
+Recall: 0.9420289855072463
+
+F1 Score: 0.962962962962963
+    
+Confusion Matrix:
+
+ [[65  1]
+ 
+ [ 4 44]]
--- WBC Classifier/bcc_model_creation.py
+++ WBC Classifier/bcc_model_creation.py
@@ -13,12 +13,11 @@ def read_data():
 columns = []
 
 
-
 def plain_testing():
     df = read_data()
     df.drop(['id', 'Unnamed: 32'], axis=1, inplace=True)
     X, y = df.drop('diagnosis', axis=1), df['diagnosis']
-    columns = X.columns
+    columns.extend(X.columns)
     x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=12)
     construct_model(x_train, x_test, y_train, y_test)
 
@@ -31,8 +30,7 @@ def model_with_min_max_scaling():
     df.drop(['id', 'Unnamed: 32'], axis=1, inplace=True)
     X, y = df.drop('diagnosis', axis=1), df['diagnosis']
     columns.extend(X.columns)
-    min_max_scaler = MinMaxScaler()
-    X = min_max_scaler.fit_transform(X)
+
     x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=12)
     construct_model(x_train, x_test, y_train, y_test)
 
@@ -41,13 +39,45 @@ def construct_model(x_train, x_test, y_train, y_test):
     classifier = DecisionTreeClassifier(random_state=0)
     classifier.fit(x_train, y_train)
 
+
+    cm = confusion_matrix(y_test, classifier.predict(x_test))
+    tp, fp, fn, tn = cm[0][0], cm[0][1], cm[1][0], cm[1][1]
+    print("Confusion Matrix:\n", cm)
+    precision, recall = tp / (tp + fp), tp / (tp + fn)
     print("Accuracy:", classifier.score(x_test, y_test))
-    print("Confusion Matrix:\n", confusion_matrix(y_test, classifier.predict(x_test)))
+    print("Precision:", precision)
+    print("Recall:", recall)
+    print("F1 Score:", 2 * (recall * precision) / (recall + precision))
+
 
     # Plot the model
-    fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(8, 8), dpi=500)
+    '''fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(8, 8), dpi=500)
     plot_tree(classifier, feature_names=columns, class_names=['M', 'B'])
-    fig.savefig('classifier_visualization.png')
+    fig.savefig('classifier_visualization.png')'''
 
 
+def run_dataset_metrics():
+    df = read_data()
+    df.drop(['id', 'Unnamed: 32'], axis=1, inplace=True)
+    X, y = df.drop('diagnosis', axis=1), df['diagnosis']
+    columns.extend(X.columns)
+    min_max_scaler = MinMaxScaler()
+    X = min_max_scaler.fit_transform(X)
+
+    percent_of_malignant = df[df['diagnosis'].str.match('M')].shape[0] / df.shape[0]
+    percent_of_benign = df[df['diagnosis'].str.match('B')].shape[0] / df.shape[0]
+    print("Percent of malignant:", percent_of_malignant * 100)
+    print("Percent of benign:", percent_of_benign * 100)
+
+    # plt.bar(['M', 'B'], [df[df['diagnosis'].str.match('M')].shape[0], df[df['diagnosis'].str.match('B')].shape[0]])
+    # plt.savefig('class_distribution.png')
+    print(X.shape,  len(columns))
+    plt.figure(figsize=(10, 10))
+    plt.boxplot(X, labels=columns, vert=True)
+    plt.xticks(fontsize=12, rotation=90)
+    plt.gcf().subplots_adjust(bottom=0.30)
+    plt.savefig('data_distribution.png')
+
+
+plain_testing()
 model_with_min_max_scaling()
